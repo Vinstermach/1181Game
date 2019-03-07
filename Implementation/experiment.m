bb1c858e4a6906fd97cc455c95b65af446ae60b1
Amarth Gul <amarthgul@gmail.com>
2019-03-07 14:26:01 -0500

    Update experiment.m

--- Implementation/experiment.m
+++ Implementation/experiment.m
@@ -15,6 +15,8 @@ function main()
         
         set(g.ui,'XData',g.x);
         set(g.ui,'YData',g.y);
+        imagesc(-g.xLen, -g.yLen, g.bgImg.resized);
+        imagesc(-g.x, -g.y, g.icon.resized);
         drawnow
     end
     
@@ -32,15 +34,23 @@ function init()
     g.gameOn = 1;
     g.bgImg.path = 'img.png';
     g.bgImg.format = 'png';
+    g.icon.path = 'icon.png';
+    g.icon.format = 'png';
+    g.icon.size = 0.002;
     g.mainFig = figure('menubar','none',...
                'numbertitle','off');
+          
     
     hold on;
     axis([-g.xLen, g.xLen, -g.yLen, g.yLen]);
     
     [g.bgImg.raw, g.bgImg.map, g.bgImg.alpha] = imread(g.bgImg.path, g.bgImg.format);
     g.bgImg.resized = imresize(g.bgImg.raw, 0.02);
-    imagesc(-g.xLen, -g.yLen, g.bgImg.resized);
+    
+    
+    [g.icon.raw, g.icon.map, g.icon.alpha] = imread(g.icon.path, g.icon.format);
+    g.icon.resized = imresize(g.icon.raw, g.icon.size);
+    
     
     g.ui = scatter(g.x, g.y, 'ro', 'filled');
     
